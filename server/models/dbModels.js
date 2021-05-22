ed7be27c08618aca65041148fa45133cb0b96d92
fenris <fenris@Chloe.local>
2021-05-21 21:09:55 -0700

    jeff got the first commit!

---
+++ server/models/dbModels.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const MONGO_URI = 'mongodb+srv://onoGrindz:onoGrindz@onogrindz.kgcfk.mongodb.net/retryWrites=true&w=majority'
+
+//mongodb+srv://onoGrindz:<password>@onogrindz.kgcfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+// establishing connection to MongoDB
+mongoose.connect(MONGO_URI, {
+  // options for the connect method to parse the URI
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // sets the name of the DB that our collections are part of
+  dbName: 'onoGrindz'
+    })
+
+  .then(() => {
+    console.log('Connected to Mongo DB.');
+  })
+  .catch((err) => {
+    console.log(`Error connecting to Mongo DB: ${err}`);
+  });
+
+  const restaurantSchema = new Schema({
+    name: { type: String, required: true },
+    display_phone: { type: String },
+    location: {
+      address1: { type: String },
+      city: { type: String },
+      zip_code:{ type: String }
+    },
+    coordinates: {
+      lat: { type: Number, required: true },
+      lng: { type: Number, required: true },
+    },
+    photos: [],
+    hours: [],
+    special_hours: [],
+  });
+
+  const userSchema = new Schema({
+    email: { type: String, required: true },
+    subscription: [restaurantSchema]
+    });
+
+const User = mongoose.model('Grindz', userSchema);
+const Restaurants = mongoose.model('Restaurants', restaurantSchema)
+
+module.exports = {
+    User,
+    Restaurants
+}
+
+//user: onoGrindz
+//pw: onoGrindz
+
+//gmail: csbigtassels@gmail.com
+//pw: codesmith
